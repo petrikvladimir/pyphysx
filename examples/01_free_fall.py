d5774149c56e0fbf798593a995e97d4c5e82e653
Vladimir Petrik <vladimir.petrik@cvut.cz>
2020-05-04 20:23:18 +0200

    free fall example updated to correspond to the example

--- examples/01_free_fall.py
+++ examples/01_free_fall.py
@@ -10,21 +10,18 @@ from pyphysx_render.rate import Rate
 from pyphysx import *
 
 scene = Scene()
-scene.add_actor(RigidStatic.create_plane(mat=Material(static_friction=0.1, dynamic_friction=0.1)))
+scene.add_actor(RigidStatic.create_plane(mat=Material(static_friction=0.1, dynamic_friction=0.1, restitution=0.5)))
 
 actor = RigidDynamic()
 actor.attach_shape(Shape.create_box([0.2] * 3, Material(restitution=1.)))
-actor.set_global_pose([0., 0.1, 1.0])
+actor.set_global_pose([0.5, 0.5, 1.0])
+actor.set_mass(1.)
 scene.add_actor(actor)
 
-actor = RigidDynamic()
-actor.attach_shape(Shape.create_box([0.1, 0.1, 0.5], Material()))
-scene.add_actor(actor)
-
-render = PyPhysXParallelRenderer()
+render = PyPhysXParallelRenderer(render_window_kwargs=dict(video_filename='out.gif'))
 
 rate = Rate(25)
-for i in range(100):
+for i in range(55):
     scene.simulate(rate.period(), 10)
     render.render_scene(scene)
     rate.sleep()
