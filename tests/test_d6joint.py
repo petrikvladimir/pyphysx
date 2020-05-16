d5525c2773ae1902a293c7ce86093c89ee786c96
Vladimir Petrik <vladimir.petrik@cvut.cz>
2020-05-16 11:27:47 +0200

    fixed test description for relative transform

--- tests/test_d6joint.py
+++ tests/test_d6joint.py
@@ -21,7 +21,11 @@ class D6JointTest(unittest.TestCase):
         self.assertAlmostEqual(j.get_local_pose(1)[0][1], -5.)
 
     def test_relative_transform(self):
-        """ T_0 = j --> a0; T_1 = j --> a1; T = T_0^-1 T_1 i.e. a0 -> j -> a1 """
+        """
+            p0 = I, p1 = I
+            T0 = (0,0.5,0), T1 = (0,0.3,0.)
+            relative transform returns value for joint required to have actors at identity
+        """
         a0, a1 = RigidDynamic(), RigidDynamic()
         j = D6Joint(a0, a1, local_pos0=(0., 3., 0.), local_pos1=(0., -5., 0.))
         self.assertAlmostEqual(j.get_relative_transform()[0][1], -8.)
