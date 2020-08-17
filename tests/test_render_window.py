75dc3f73a9099f3c45b1a7d0ba5d96cf36cdba28
Vladimír Petrík <vladko.petrik@gmail.com>
2020-08-17 12:42:21 +0200

    Move camera (#28)
    Added camera manipulation and testing script for pyglet renderer.
    
    Co-authored-by: Vladimir Petrik <vladimir.petrik@cvut.cz>

---
+++ tests/test_render_window.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Copyright (c) CTU  - All Rights Reserved
+# Created on: 8/5/20
+#     Author: Vladimir Petrik <vladimir.petrik@cvut.cz>
+
+import unittest
+from queue import Queue
+import numpy as np
+
+from pyglet.window.key import LEFT, RIGHT, UP, DOWN
+from pyphysx_render.render_window import PyPhysXWindow
+
+
+class RenderWindowTestCase(unittest.TestCase):
+
+    def test_render_look_at_key_modifications(self):
+        self.queue = Queue()
+        window = PyPhysXWindow(queue=self.queue)
+        self.assertAlmostEqual(0., np.linalg.norm(window.look_at))
+        window.on_key_press(LEFT, None)
+        self.assertAlmostEqual(1e-1, np.linalg.norm(window.look_at))
+        self.assertNotAlmostEqual(0., np.linalg.norm(window.look_at))
+        window.on_key_press(RIGHT, None)
+        self.assertAlmostEqual(0., np.linalg.norm(window.look_at))
+
+        self.assertAlmostEqual(0., np.linalg.norm(window.look_at))
+        window.on_key_press(UP, None)
+        self.assertAlmostEqual(1e-1, np.linalg.norm(window.look_at))
+        self.assertNotAlmostEqual(0., np.linalg.norm(window.look_at))
+        window.on_key_press(DOWN, None)
+        self.assertAlmostEqual(0., np.linalg.norm(window.look_at))
+
+
+if __name__ == '__main__':
+    unittest.main()
