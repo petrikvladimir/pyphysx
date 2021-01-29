737159083e55bd9bec841310ac4c2ca5034f00ba
Vladimir Petrik <vladimir.petrik@cvut.cz>
2021-01-29 11:14:28 +0100

    test pyrender added and version increased. Resolves #37.

--- pyphysx_render/pyrender_base.py
+++ pyphysx_render/pyrender_base.py
@@ -7,7 +7,8 @@
 from typing import List
 
 import numpy as np
-import trimesh
+from trimesh import Trimesh
+from trimesh.creation import uv_sphere, box
 
 from pyrender.scene import Scene as PyRenderScene
 from pyrender import PerspectiveCamera, Mesh, Primitive, GLTF
@@ -87,60 +88,63 @@ class PyRenderBase(ViewerBase):
                 self.render_scene.set_pose(node, self._get_actor_pose_matrix(actor, offset))
             self._release_lock()
 
-    def shape_to_meshes(self, shape: Shape):
-        clr_string = shape.get_user_data().get('color', None) if shape.get_user_data() is not None else None
-        clr = gl_color_from_matplotlib(color=clr_string, return_rgba=True)
-        visual_mesh = shape.get_user_data().get('visual_mesh', None) if shape.get_user_data() is not None else None
-
-        meshes = []
-        if visual_mesh is not None:
-            if isinstance(visual_mesh, trimesh.Trimesh):
+    def _trimesh_from_basic_shape(self, shape: Shape, vertex_colors=None):
+        """ Get trimesh from shape for visual meshes, spheres, or boxes. Return None if it is not a basic shape. """
+        if shape.get_user_data() is not None:
+            visual_mesh = shape.get_user_data().get('visual_mesh', None)
+            if visual_mesh is not None:
+                if not isinstance(visual_mesh, Trimesh):
+                    raise NotImplementedError('Only trimesh scene or trimesh instances are supported.')
                 if hasattr(visual_mesh.visual, 'material'):
                     visual_mesh.visual.material.kwargs['Ns'] = np.abs(visual_mesh.visual.material.kwargs['Ns'])
-                meshes.append(Mesh.from_trimesh(visual_mesh))
-            else:
-                raise NotImplementedError('Only trimesh scene or trimesh instances are supported.')
-        elif shape.get_geometry_type() == GeometryType.SPHERE:
-            geom = trimesh.creation.uv_sphere(radius=shape.get_sphere_radius(), count=self.spheres_count)
-            geom.visual.vertex_colors = clr
-            meshes.append(Mesh.from_trimesh(geom))
-        elif shape.get_geometry_type() == GeometryType.BOX:
-            geom = trimesh.creation.box(extents=2 * shape.get_box_half_extents())
-            geom.visual.vertex_colors = clr
-            meshes.append(Mesh.from_trimesh(geom))
-        elif shape.get_geometry_type() == GeometryType.PLANE:
-            d = self.plane_grid_spacing
-            n = self.plane_grid_num_of_lines
-            num_horizontal_lines = 2 * n + 1
-            points = np.zeros((2 * 2 * num_horizontal_lines, 3))
-            g = np.linspace(-n * d, n * d, num_horizontal_lines)
-            points[:2 * num_horizontal_lines, 2] = g.repeat(2)
-            points[:2 * num_horizontal_lines, 1] = np.tile([-n * d, n * d], num_horizontal_lines)
-            points[2 * num_horizontal_lines:, 2] = np.tile([-n * d, n * d], num_horizontal_lines)
-            points[2 * num_horizontal_lines:, 1] = g.repeat(2)
-            clr = [0.8] * 3 + [0.1] if clr_string is None else gl_color_from_matplotlib(clr_string, return_rgba=True)
-            primitive = Primitive(positions=points, normals=None, color_0=clr, mode=GLTF.LINES, poses=None)
-            meshes.append(Mesh(primitives=[primitive]))
+                return visual_mesh
+        gtype = shape.get_geometry_type()
+        if not gtype in [GeometryType.SPHERE, GeometryType.BOX]:
+            return None
+        geom = uv_sphere(shape.get_sphere_radius(), self.spheres_count) if gtype == GeometryType.SPHERE else box(
+            2 * shape.get_box_half_extents())
+        if vertex_colors is not None:
+            geom.visual.vertex_colors = vertex_colors
+        return geom
+
+    def _grid_lines(self):
+        """ Get points for n lines [Nx3] representing grid structure. Grid is in y-z plane.
+         However, PyRender renders it on xy plane. """
+        d = self.plane_grid_spacing
+        n = self.plane_grid_num_of_lines
+        num_horizontal_lines = 2 * n + 1
+        points = np.zeros((2 * 2 * num_horizontal_lines, 3))
+        g = np.linspace(-n * d, n * d, num_horizontal_lines)
+        points[:2 * num_horizontal_lines, 2] = g.repeat(2)
+        points[:2 * num_horizontal_lines, 1] = np.tile([-n * d, n * d], num_horizontal_lines)
+        points[2 * num_horizontal_lines:, 2] = np.tile([-n * d, n * d], num_horizontal_lines)
+        points[2 * num_horizontal_lines:, 1] = g.repeat(2)
+        return points
+
+    def shape_to_mesh(self, shape: Shape):
+        """ Convert pyphysx shape into the pyrender Mesh """
+        clr_string = shape.get_user_data().get('color', None) if shape.get_user_data() is not None else None
+        clr = gl_color_from_matplotlib(color=clr_string, return_rgba=True)
+        basic_trimesh = self._trimesh_from_basic_shape(shape, clr)
+        if basic_trimesh is not None:
+            return Mesh.from_trimesh(basic_trimesh)
         elif shape.get_geometry_type() == GeometryType.CONVEXMESH:
             data = shape.get_shape_data()  # N x 9 - i.e. 3 triangles
-            primitive = Primitive(positions=data.reshape(-1, 3), normals=None, color_0=clr, mode=GLTF.TRIANGLES,
-                                  poses=None)
-            meshes.append(Mesh(primitives=[primitive]))
+            primitive = Primitive(data.reshape(-1, 3), normals=None, color_0=clr, mode=GLTF.TRIANGLES, poses=None)
+            return Mesh(primitives=[primitive])
+        elif shape.get_geometry_type() == GeometryType.PLANE:
+            clr = [0.8] * 3 + [0.1] if clr_string is None else gl_color_from_matplotlib(clr_string, return_rgba=True)
+            primitive = Primitive(positions=self._grid_lines(), normals=None, color_0=clr, mode=GLTF.LINES, poses=None)
+            return Mesh(primitives=[primitive])
         else:
             raise NotImplementedError('Not supported type of the geometry.')
-        return meshes
 
-    def shape_to_nodes(self, shape):
+    def shape_to_node(self, shape):
         pose = pose_to_transformation_matrix(shape.get_local_pose())
-        return [Node(mesh=mesh, matrix=pose) for mesh in self.shape_to_meshes(shape=shape)]
+        return Node(mesh=self.shape_to_mesh(shape=shape), matrix=pose)
 
     def actor_to_node(self, actor, flags):
         shapes = [s for s in actor.get_atached_shapes() if PyRenderBase.has_shape_any_of_flags(s, flags)]
         if len(shapes) == 0:
             return None
-
-        # todo: simplify to single line!
-        all_nodes: List[Node] = []
-        for s in shapes:
-            all_nodes += self.shape_to_nodes(s)
-        return Node(children=all_nodes)
+        return Node(children=[self.shape_to_node(s) for s in shapes])
---
+++ tests/test_pyrender.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+# Copyright (c) CTU  - All Rights Reserved
+# Created on: 1/29/21
+#     Author: Vladimir Petrik <vladimir.petrik@cvut.cz>
+
+import numpy as np
+import os
+import unittest
+from trimesh.creation import uv_sphere
+
+from pyphysx import *
+from pyphysx_render.pyrender_base import PyRenderBase
+from pyphysx_render.pyrender_offscreen_renderer import PyPhysxOffscreenRenderer
+
+os.environ['PYOPENGL_PLATFORM'] = 'egl'  # first set backend to headless mode
+
+
+class TestPyRender(unittest.TestCase):
+    def test_base_scene_creation(self):
+        r = PyRenderBase()
+        """ Default bg color """
+        self.assertEqual(r.render_scene.bg_color[0], 0.75)
+
+        """ Default camera position """
+        pose = r.render_scene.get_pose(r.render_scene.main_camera_node)
+        self.assertAlmostEqual(pose[0, 3], 3. * np.sin(np.deg2rad(45)))
+        self.assertAlmostEqual(pose[1, 3], 0.)
+        self.assertAlmostEqual(pose[2, 3], 3. * np.cos(np.deg2rad(45)))
+
+        """ no actors """
+        self.assertEqual(len(r.nodes_and_actors), 0)
+
+    def test_scene_size(self):
+        scene = Scene()
+        actor = RigidDynamic()
+        actor.attach_shape(Shape.create_box([0.2] * 3, Material()))
+        scene.add_actor(actor)
+
+        r = PyRenderBase()
+        r.add_physx_scene(scene)
+        self.assertEqual(len(r.nodes_and_actors), 1)
+        r.clear_physx_scenes()
+        self.assertEqual(len(r.nodes_and_actors), 0)
+
+    def test_actor_pose_matrix(self):
+        scene = Scene()
+        actor = RigidDynamic()
+        actor.attach_shape(Shape.create_box([0.2] * 3, Material()))
+        actor.set_global_pose([1, 2, 3])
+        scene.add_actor(actor)
+        p1 = PyRenderBase._get_actor_pose_matrix(actor, offset=None)
+        p2 = PyRenderBase._get_actor_pose_matrix(actor, offset=[3., 2., 1.])
+        self.assertEqual(p1.shape, (4, 4))
+        self.assertEqual(p2.shape, (4, 4))
+        self.assertAlmostEqual(p1[0, 3], 1.)
+        self.assertAlmostEqual(p1[1, 3], 2.)
+        self.assertAlmostEqual(p1[2, 3], 3.)
+        self.assertAlmostEqual(p2[0, 3], 4.)
+        self.assertAlmostEqual(p2[1, 3], 4.)
+        self.assertAlmostEqual(p2[2, 3], 4.)
+
+    def test_update(self):
+        scene = Scene()
+        actor = RigidDynamic()
+        actor.attach_shape(Shape.create_box([0.2] * 3, Material()))
+        actor.set_global_pose([1, 2, 3])
+        scene.add_actor(actor)
+        r = PyRenderBase()
+        r.add_physx_scene(scene)
+        r.update()
+        p1 = r.render_scene.get_pose(r.nodes_and_actors[0][0])
+        self.assertAlmostEqual(p1[0, 3], 1.)
+        self.assertAlmostEqual(p1[1, 3], 2.)
+        self.assertAlmostEqual(p1[2, 3], 3.)
+        actor.set_global_pose([4, 5, 6])
+        p1 = r.render_scene.get_pose(r.nodes_and_actors[0][0])
+        self.assertAlmostEqual(p1[0, 3], 1.)
+        self.assertAlmostEqual(p1[1, 3], 2.)
+        self.assertAlmostEqual(p1[2, 3], 3.)
+        r.update()
+        p1 = r.render_scene.get_pose(r.nodes_and_actors[0][0])
+        self.assertAlmostEqual(p1[0, 3], 4.)
+        self.assertAlmostEqual(p1[1, 3], 5.)
+        self.assertAlmostEqual(p1[2, 3], 6.)
+
+    def test_shape_any_of_flags(self):
+        s = Shape.create_box([0.2] * 3, Material())
+        s.set_flag(ShapeFlag.VISUALIZATION, True)
+        s.set_flag(ShapeFlag.SIMULATION_SHAPE, True)
+        s.set_flag(ShapeFlag.SCENE_QUERY_SHAPE, False)
+        self.assertTrue(PyRenderBase.has_shape_any_of_flags(s, [ShapeFlag.VISUALIZATION]))
+        self.assertTrue(PyRenderBase.has_shape_any_of_flags(s, [ShapeFlag.SIMULATION_SHAPE]))
+        self.assertTrue(PyRenderBase.has_shape_any_of_flags(s, [ShapeFlag.SIMULATION_SHAPE, ShapeFlag.VISUALIZATION]))
+        self.assertTrue(PyRenderBase.has_shape_any_of_flags(s, [ShapeFlag.VISUALIZATION, ShapeFlag.SCENE_QUERY_SHAPE]))
+        self.assertFalse(PyRenderBase.has_shape_any_of_flags(s, [ShapeFlag.SCENE_QUERY_SHAPE]))
+        self.assertFalse(PyRenderBase.has_shape_any_of_flags(s, []))
+
+    def test_grid_lines(self):
+        r = PyRenderBase()
+        lines = r._grid_lines()
+        self.assertEqual(lines.shape, (21 * 4, 3))
+        self.assertAlmostEqual(lines[0, 1], -10.)
+        self.assertAlmostEqual(lines[1, 1], 10.)
+        self.assertAlmostEqual(lines[-2, 2], -10.)
+        self.assertAlmostEqual(lines[-1, 2], 10.)
+
+    def test_basic_shape_trimesh(self):
+        r = PyRenderBase(spheres_count=(4, 4))
+        s = Shape.create_box([0.2] * 3, Material())
+        m = r._trimesh_from_basic_shape(s, vertex_colors=[10, 20, 30, 250])
+        self.assertEqual(m.visual.vertex_colors.shape, (8, 4))  # box has 8 vertices
+        self.assertEqual(m.visual.vertex_colors[5, 0], 10)
+        self.assertEqual(m.visual.vertex_colors[5, 1], 20)
+        self.assertEqual(m.visual.vertex_colors[5, 2], 30)
+        self.assertEqual(m.visual.vertex_colors[5, 3], 250)
+
+        s = Shape.create_sphere(0.15, Material())
+        m = r._trimesh_from_basic_shape(s)
+        err = np.max(np.abs(np.linalg.norm(m.vertices, axis=-1) - 0.15))  # error of sphere creation
+        self.assertAlmostEqual(err, 0.)
+
+        s.set_user_data({'visual_mesh': uv_sphere(1.)})  # set user data - should return this mesh instead of shape
+        m = r._trimesh_from_basic_shape(s)
+        err = np.max(np.abs(np.linalg.norm(m.vertices, axis=-1) - 1.))  # error of sphere creation
+        self.assertAlmostEqual(err, 0.)
+
+    def test_actor_to_node(self):
+        r = PyRenderBase()
+
+        actor = RigidDynamic()
+        self.assertEqual(r.actor_to_node(actor, [ShapeFlag.VISUALIZATION]), None)
+
+        actor.attach_shape(Shape.create_box([0.2] * 3, Material()))
+        n = r.actor_to_node(actor, [ShapeFlag.VISUALIZATION])
+        self.assertEqual(len(n.children), 1)
+
+        actor.attach_shape(Shape.create_box([0.2] * 3, Material()))
+        n = r.actor_to_node(actor, [ShapeFlag.VISUALIZATION])
+        self.assertEqual(len(n.children), 2)
+
+    def test_offscreen_renderer_shapes(self):
+        r = PyPhysxOffscreenRenderer(viewport_size=(320, 240))
+        rgb, depth = r.get_rgb_and_depth()
+        self.assertEqual(rgb.shape, (240, 320, 3))
+        self.assertEqual(depth.shape, (240, 320))
+        rgba, depth = r.get_rgba_and_depth()
+        self.assertEqual(rgba.shape, (240, 320, 4))
+        self.assertEqual(depth.shape, (240, 320))
+        depth = r.get_depth()
+        self.assertEqual(depth.shape, (240, 320))
+
+
+if __name__ == '__main__':
+    unittest.main()
