0baac11887f1a99d9dff371602b869fecfbb1637
Vladimir Petrik <vladimir.petrik@cvut.cz>
2021-10-01 14:48:16 +0200

    adding cmake build type to windows and workflow without conan center

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ class CMakeBuild(build_ext):
 
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             if sys.maxsize > 2 ** 32:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
